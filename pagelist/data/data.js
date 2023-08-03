72c283aca64493700eb103f071a86f4cbd47568c
gotaeyoon <ty.go.payple.kr>
2023-08-03 18:07:03 +0900

    작업 환경 세팅 진행중

---
+++ pagelist/data/data.js
@@ -0,0 +1,43 @@
+const data = {}
+data.guide = {
+    title: '가이드',
+    list: [
+        {
+            stateClass: 'working',
+            url: 'layout',
+            category: 'guide',
+            page: 'layout'
+        },
+        {
+            stateClass: 'working',
+            url: 'button',
+            category: 'guide',
+            page: 'button'
+        },
+        {
+            stateClass: 'working',
+            url: 'calender',
+            category: 'guide',
+            page: 'calender'
+        },
+        {
+            stateClass: 'working',
+            url: 'table',
+            category: 'guide',
+            page: 'table'
+        },
+        {
+            stateClass: 'working',
+            url: 'modal',
+            category: 'guide',
+            page: 'modal'
+        },
+
+    ]
+}
+
+
+
+
+
+
