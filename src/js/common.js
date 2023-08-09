d0270d03b8bcf4197c3fcc69d2bf9a1a6ce03dca
gotaeyoon <ty.go.payple.kr>
2023-08-10 08:52:34 +0900

    aside jquery 작업 완료

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,18 @@
 function asideSlideBtn(){
+    let title = $('._title');
+    let slide = $('._slide');
 
+    title.on('click',function(){
+        $(this).next(slide).stop().slideToggle(300);
+        $(this).toggleClass('active').parent().siblings().removeClass('active');
+        console.log($(this).parent().siblings().find(title).removeClass('active'));
+        $(this).parent().siblings().find(slide).slideUp(300);
+    })
+
+
+
+
+    $(this).next(".anw").siblings(".anw").slideUp(300);
 }
 
 $(document).ready(function(){
