5dd1c249dab6c4e645577f307d1e9f590717cec5
gotaeyoon <ty.go.payple.kr>
2023-09-08 18:01:03 +0900

    수정 작업 진행중

--- src/js/common.js
+++ src/js/common.js
@@ -57,7 +57,7 @@ function dataRangePicker() {
             autoApply: true,
             "locale": {
                 "format": "YYYY-MM-DD",
-                "separator": " ~ ",
+                "separator": "~",
                 "weekLabel": "주",
                 "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
                 "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
@@ -71,7 +71,7 @@ function dataRangePicker() {
             "parentEl": modalDataPicker,
         },
         function (start, end, label) {
-            console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+
         });
     dateRangePicker.on('show.daterangepicker', function (ev, picker) {
         $('#myModal').modal('show')
