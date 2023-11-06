45ede473a6aa2bece98f667ad078836a81c7bfbc
gotaeyoon <ty.go.payple.kr>
2023-11-06 13:55:21 +0900

    single 달력 수정 완료

--- src/js/common.js
+++ src/js/common.js
@@ -91,9 +91,7 @@ function showRangeDateModal() {
             "showCustomRangeLabelranges": false,
             "parentEl": modalRangeDate,
         },
-        function (start, end, label) {
-
-        });
+    );
     inputRangeDate.on('hide.daterangepicker', function (ev, picker) {
         $('._modal-calendar').modal('hide')
     });
@@ -141,6 +139,22 @@ function showRangeDateModal() {
 
 
 function showSingleDateModal(){
+    const modalSingleDate = $('._modalSingleDate');
+    const inputSingleDate = $('._inputSingleDate');
+    inputSingleDate.daterangepicker(
+        {
+            singleDatePicker: true,
+            linkedCalendars: true,
+            autoApply: true,
+            "locale": {
+                "format": "YYYY-MM-DD",
+                "separator": "~",
+                "weekLabel": "주",
+                "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
+                "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+            },
+        },
+    );
 
 }
 
